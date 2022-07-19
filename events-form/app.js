66969676ea54c655159b307c743f7aa232193f65
josimara-lima2 <josybehlul@gmail.com>
2022-07-19 10:12:13 -0300

    exercicio 17

--- events-form/app.js
+++ events-form/app.js
@@ -6,3 +6,20 @@ form.addEventListener('submit', event => {
     console.log(form.username.value)
    // console.log(event.target.username.value)
 })
+
+
+//Expressão regular
+const username = 'josimara'
+// Letras minúsculas e pelo menos 6 caracteres
+const pattern = /^[a-z]{6,}$/
+// const isAMatch = pattern.test('username')
+// console.log(isAMatch)
+// if(isAMatch){
+//     console.log('o teste da regex passou')
+// }else{
+//     console.log('o teste da regex não passou')
+// }
+
+// se o match nao acontece retorna -1
+const result = username.search(pattern)
+console.log(result)
